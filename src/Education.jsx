9dab8f1f73d2795437dab67f0e8befbc31e77b83
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-08-04 11:36:08 +0530

    feat!: add editing feature to each education entry

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,32 +1,96 @@
+import { useState } from "react";
+
 export function Education({ education, setEducation }) {
+  const [editId, setEditId] = useState(null);
+  const [formData, setFormData] = useState({
+    college: "",
+    degree: "",
+    location: "",
+    time: "",
+  });
+
+  const isEditing = editId !== null;
+
+  function formHandler(e) {
+    const formElementName = e.target.name;
+    const formElementValue = e.target.value;
+    setFormData({ ...formData, [formElementName]: formElementValue });
+  }
+
   function submitHandler(e) {
     e.preventDefault();
+    if (isEditing) {
+      setEducation(
+        education.map((entry) =>
+          entry.id === editId ? { ...entry, ...formData } : entry
+        )
+      );
+      setEditId(null);
+    } else {
+      const eduEntry = {
+        id: crypto.randomUUID(),
+        ...formData,
+      };
+      setEducation([...education, eduEntry]);
+    }
 
-    const form = e.target;
+    setFormData({
+      college: "",
+      degree: "",
+      location: "",
+      time: "",
+    });
+  }
 
-    const eduEntry = {
-      id: crypto.randomUUID(),
-      college: form.college.value,
-      degree: form.degree.value,
-      location: form.location.value,
-      time: form.time.value,
-    };
+  function handleEdit(entry) {
+    setEditId(entry.id);
+    setFormData(entry);
+  }
 
-    setEducation([...education, eduEntry]);
+  function handleCancelEdit() {
+    setEditId(null);
+    setFormData({ college: "", degree: "", location: "", time: "" });
   }
 
   function handleDelete(id) {
+    console.log(id, editId);
+
+    // just to check if its the same entry that's currently user editing
+
+    if (id === editId) {
+      setFormData({
+        college: "",
+        degree: "",
+        location: "",
+        time: "",
+      });
+      setEditId(null);
+    }
+
     const updatedEducationList = education.filter((entry) => entry.id !== id);
     setEducation(updatedEducationList);
   }
 
   return (
     <>
-      <h2>Education Details: </h2>
+      <h2>Education Details</h2>
+
       <br />
-      <EducationForm submitHandler={submitHandler} />
+
+      <EducationForm
+        formHandler={formHandler}
+        formData={formData}
+        submitHandler={submitHandler}
+        isEditing={isEditing}
+        handleCancelEdit={handleCancelEdit}
+      />
+
       <br />
+
       <p>Submitted Education:</p>
+
+      <br />
+
       {education.map((educationEntry) => {
         return (
           <div key={educationEntry.id}>
@@ -38,7 +102,7 @@ export function Education({ education, setEducation }) {
             <button onClick={() => handleDelete(educationEntry.id)}>
               Delete
             </button>
-            <button>Edit</button>
+            <button onClick={() => handleEdit(educationEntry)}>Edit</button>
           </div>
         );
       })}
@@ -46,28 +110,63 @@ export function Education({ education, setEducation }) {
   );
 }
 
-function EducationForm({ submitHandler }) {
+function EducationForm({
+  isEditing,
+  submitHandler,
+  formData,
+  formHandler,
+  handleCancelEdit,
+}) {
   return (
     <>
       <form onSubmit={submitHandler}>
         <label>
           College:
-          <input type="text" name="college" />
-        </label>
-        <label>
-          Degree:
-          <input type="text" name="degree" />
-        </label>
-        <label>
-          Location:
-          <input type="text" name="location" />
-        </label>
-        <label>
-          Time:
-          <input type="text" name="time" />
+          <input
+            type="text"
+            onChange={formHandler}
+            value={formData.college}
+            name="college"
+          />
         </label>
 
-        <button type="submit">Submit</button>
+        <label>
+          Degree:
+          <input
+            type="text"
+            onChange={formHandler}
+            value={formData.degree}
+            name="degree"
+          />
+        </label>
+
+        <label>
+          Location:
+          <input
+            type="text"
+            name="location"
+            onChange={formHandler}
+            value={formData.location}
+          />
+        </label>
+
+        <label>
+          Time:
+          <input
+            type="text"
+            onChange={formHandler}
+            name="time"
+            value={formData.time}
+          />
+        </label>
+
+        <button type="submit">{isEditing ? "Save" : "Submit"}</button>
+
+        {isEditing && (
+          <button onClick={handleCancelEdit} type="button">
+            Cancel
+          </button>
+        )}
       </form>
     </>
   );
