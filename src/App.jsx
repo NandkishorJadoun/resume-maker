83c335c574b92ef5a184ce91b2ab91fc279305b5
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-08-06 20:40:00 +0530

    refactor!: structure the state

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import { Name } from "./Name";
 import { Contact } from "./Contact";
 import { Education } from "./Education";
+import { Skills } from "./Skills";
+import { Experience } from "./Experience";
+import { Projects } from "./Projects";
 
 function App() {
-
   const [name, setName] = useState({ first: "", last: "" });
   const [contact, setContact] = useState({
     number: "",
@@ -15,12 +17,11 @@ function App() {
   const [education, setEducation] = useState([]);
   const [experience, setExperience] = useState([]);
   const [projects, setProjects] = useState([]);
-  const [skills, setSkills] = useState({
-    languages: [],
-    frameworks: [],
-    tools: [],
-    libraries: [],
-  });
+
+  const [languages, setLanguages] = useState([]);
+  const [frameworks, setFrameworks] = useState([]);
+  const [tools, setTools] = useState([]);
+  const [libraries, setLibraries] = useState([]);
 
   const resumeData = {
     name,
@@ -28,7 +29,7 @@ function App() {
     education,
     experience,
     projects,
-    skills,
+    skills: { languages, frameworks, tools, libraries },
   };
 
   return (
@@ -40,6 +41,21 @@ function App() {
       <Contact contact={contact} setContact={setContact} />
       <hr />
       <Education education={education} setEducation={setEducation} />
+      <hr />
+      <Experience experience={experience} setExperience={setExperience} />
+      <hr />
+      <Projects projects={projects} setProjects={setProjects} />
+      <hr />
+      <Skills
+        languages={languages}
+        setLanguages={setLanguages}
+        frameworks={frameworks}
+        setFrameworks={setFrameworks}
+        tools={tools}
+        setTools={setTools}
+        libraries={libraries}
+        setLibraries={setLibraries}
+      />
       <hr />
       <Preview resumeData={resumeData} />
     </>
